1928a99ea95484131e7b3042bef98dd6f6af43e0
Hotmoon <aravind1333.av@gmail.com>
2023-06-25 16:43:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = 12581928
-    API_HASH = '9bd003806267466f904f91aa035d8991'
-    BOT_TOKEN = '5308277473:AAHcoxK_BtfycMCCH5uu9YceCXb8pG0AxVA'
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1287276743
-    OWNER = 1621366244
+    OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = 'https://te.legra.ph/file/cc6cb5448aee307c8f16e.jpg'
+    THUMB = config("THUMBNAIL")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
